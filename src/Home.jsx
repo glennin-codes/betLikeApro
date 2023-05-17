3cfde97d046afb057a800165c650bff152ad6901
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-05-17 08:55:55 +0300

    revumped code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import axios from 'axios';
-import { Button, Card, CardContent, Grid, Typography, Divider } from '@mui/material';
+import { Button, Card, CardContent, Grid, Typography, Divider, Snackbar } from '@mui/material';
 import { test } from './test';
 
 const Root = styled.div`
@@ -28,9 +28,11 @@ function Home() {
   const [matches, setMatches] = useState([]);
   const [today, setToday] = useState('');
   const [tomorrow, setTomorrow] = useState('');
+ 
+  const [predictionResult, setPredictionResult] = useState(null);
+  const [showPredictionToast, setShowPredictionToast] = useState(false);
 
   useEffect(() => {
-  
     const today = new Date();
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1);
@@ -38,11 +40,7 @@ function Home() {
     setTomorrow(formatDate(tomorrow));
 
     axios.get('http://localhost:5000/matches').then((response) => {
-      console.log("original",response);
-   
-    const { data } = response;
-       console.log("original",data);
-      
+      const { data } = response;
       setMatches(
         data.filter((match) => {
           const matchDate = new Date(match.matchTime);
@@ -52,12 +50,27 @@ function Home() {
     });
   }, []);
 
-  const handlePrediction = ({homeTeamID,awayTeamId}) => {
-    
-
-    axios.post('http://localhost:5000/predict', { home_team_id: homeTeamID, away_team_id: awayTeamId }).then((response) => {
-      console.log(response.data.prediction);
+  const handlePrediction = (match) => {
+    const { homeTeamId, awayTeamId } = match;
+    axios.post('http://localhost:5000/predict', { home_team_id: homeTeamId, away_team_id: awayTeamId }).then((response) => {
+      const prediction = response.data.prediction;
+      if (prediction===0){
+            
+      setPredictionResult(`${homeTeamName} has a high chances of winning`);
+      }
+      else if(prediction===1){
+        setPredictionResult(`${awayTeamName} high chances of winning`)
+      }else if(prediction===0){
+        setPredictionResult('mostly going to a Draw')
+      }else{
+        setPredictionResult('undocumented,I may miss some old data')
+      }
+      setShowPredictionToast(true);
     });
+  };
+
+  const handlePredictionToastClose = () => {
+    setShowPredictionToast(false);
   };
 
   return (
@@ -71,7 +84,6 @@ function Home() {
       </Typography>
       <Grid container spacing={2} justify="center">
         {matches.map((match) => (
-           
           <Grid item key={match.matchId}>
             <StyledCard>
               <CardContent>
@@ -109,103 +121,14 @@ function Home() {
       <Typography variant="h6" align="center" gutterBottom>
         {tomorrow}
       </Typography>
+      <Snackbar
+        open={showPredictionToast}
+        autoHideDuration={3000}
+        onClose={handlePredictionToastClose}
+        message={`Prediction: ${predictionResult}`}
+      />
     </Root>
   );
 }
 
 export default Home;
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import styled from '@emotion/styled';
-// import axios from 'axios';
-// import { Button, Card, CardContent, Grid, Typography, Divider } from '@mui/material';
-// import { test } from './test';
-
-// const Root = styled.div`
-//   padding: ${({ theme }) => theme.spacing(2)};
-// `;
-
-// const StyledCard = styled(Card)`
-//   margin: ${({ theme }) => theme.spacing(2)};
-// `;
-
-// function formatDate(date) {
-//   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-//   return date.toLocaleDateString(undefined, options);
-// }
-
-// function Home() {
-//   const [matches, setMatches] = useState([]);
-//   const [today, setToday] = useState('');
-//   const [tomorrow, setTomorrow] = useState('');
-
-//   useEffect(() => {
-//     test();
-//     const today = new Date();
-//     const tomorrow = new Date(today);
-//     tomorrow.setDate(today.getDate() + 1);
-//     setToday(formatDate(today));
-//     setTomorrow(formatDate(tomorrow));
-
-//     axios.get('http://localhost:5000/matches').then((response) => {
-//       const {data}=response
-//       setMatches(data.matches.filter(match => {
-//         const matchDate = new Date(match.utcDate);
-//         return matchDate >= today && matchDate < tomorrow;
-//       }));
-//     });
-//   }, []);
-
-//   const handlePrediction = (matchId) => {
-//     axios.post('http://localhost:5000/predict', { matchId }).then((response) => {
-//       console.log(response.data.prediction);
-//     });
-//   };
-
-//   return (
-//     <Root>
-//       <Typography variant="h4" align="center" gutterBottom>
-//         Matches Today
-//       </Typography>
-//       <Divider />
-//       <Typography variant="h6" align="center" gutterBottom>
-//         {today}
-//       </Typography>
-//       <Grid container spacing={2} justify="center">
-//         {matches.map((match) => (
-//           <Grid item key={match.id}>
-//             <StyledCard>
-//               <CardContent>
-//                 <Typography variant="h6" component="h2">
-//                   {match.homeTeam.name} vs {match.awayTeam.name}
-//                 </Typography>
-//                 <Typography color="textSecondary" gutterBottom>
-//                   {match.competition.name}
-//                 </Typography>
-//                 <Typography variant="body2" component="p">
-//                   {new Date(match.utcDate).toLocaleString()}
-//                 </Typography>
-//                 <Button
-//                   variant="contained"
-//                   color="primary"
-//                   onClick={() => handlePrediction(match.id)}
-//                 >
-//                   Predict
-//                 </Button>
-//               </CardContent>
-//             </StyledCard>
-//           </Grid>
-//         ))}
-//       </Grid>
-//       <Typography variant="h6" align="center" gutterBottom>
-//         {tomorrow}
-//       </Typography>
-//     </Root>
-//   );
-// }
-
-// export default Home;
