8db35ead4273762388710c3112a871b5a54b179a
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-05-17 16:43:34 +0300

    skeleteon feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,8 +10,8 @@ import {
   Divider,
   Snackbar,
 } from "@mui/material";
-import MuiAlert from '@mui/material/Alert';
-
+import { Skeleton } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 const Root = styled.div`
   padding: ${({ theme }) => theme.spacing(2)};
@@ -42,6 +42,7 @@ function Home() {
   const [matches, setMatches] = useState([]);
   const [today, setToday] = useState("");
   const [tomorrow, setTomorrow] = useState("");
+  const [matchLoading, setMatchLoading] = useState(false);
   const [error, setError] = useState(null);
   const [predictionResult, setPredictionResult] = useState(null);
   const [showPredictionToast, setShowPredictionToast] = useState(false);
@@ -52,11 +53,12 @@ function Home() {
     tomorrow.setDate(today.getDate() + 1);
     setToday(formatDate(today));
     setTomorrow(formatDate(tomorrow));
-
+    setMatchLoading(true);
     axios
       .get("http://localhost:5000/matches")
       .then((response) => {
         const { data } = response;
+        setMatchLoading(false);
         setMatches(
           data.filter((match) => {
             const matchDate = new Date(match.matchTime);
@@ -70,7 +72,6 @@ function Home() {
         } else {
           setError("Network error. Please check your internet connection.");
         }
-        setIsLoading(false);
       });
   }, []);
 
@@ -105,7 +106,6 @@ function Home() {
       });
   };
 
-
   const handleClosePredictionToast = () => {
     setShowPredictionToast(false);
     setPredictionResult("");
@@ -120,65 +120,235 @@ function Home() {
       <Typography variant="h6" align="center" gutterBottom>
         {today}
       </Typography>
-      <Grid container spacing={2} justify="center">
-        {matches.map((match) => (
-          <Grid item key={match.matchId}>
-            <StyledCard>
-              <CardContent>
-                <Grid container alignItems="center" spacing={2}>
-                  <Grid item>
-                    <LogoImage
-                      src={match.homeTeamLogo}
-                      alt={match.homeTeamName}
-                    />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" component="h2">
-                      {match.homeTeamName} vs {match.awayTeamName}
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <LogoImage
-                      src={match.awayTeamLogo}
-                      alt={match.awayTeamName}
-                    />
-                  </Grid>
-                </Grid>
-                <Typography color="textSecondary" gutterBottom>
-                  {match.competitionName}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {new Date(match.matchTime).toLocaleString()}
-                </Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => handlePrediction(match)}
+      {matchLoading ? (
+        <>
+          <Grid 
+            
+          >
+            <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem'}}>
+              <CardContent sx={{ fontSize: "3rem" }}>
+                {/* Skeleton for the logo */}
+
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
                 >
-                  Predict
-                </Button>
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Grid>
+                <Grid item
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {/* Skeleton for the team names */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    
+                    style={{ marginTop: "8px" ,paddingRight:'10'}}
+                  />
+
+                  {/* Skeleton for the competition name */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    style={{ marginTop: "8px" }}
+                  />
+                </Grid>
+                <Grid item
+                xs={12}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                
+                }}
+                >
+                {/* Skeleton for the match time */}
+                <Skeleton
+                  variant="text"
+                  width={280}
+                  height={12}
+                  style={{ marginTop: "4px" }}
+                />
+
+                {/* Skeleton for the Predict button */}
+                <Skeleton
+                  variant="rectangular"
+                  width={280}
+                  height={36}
+                  style={{ marginTop: "8px" }}
+                />
+                </Grid>
+              </CardContent>
+            </StyledCard>
+            <StyledCard sx={{ height: 150, width: 300 }}>
+              <CardContent sx={{ fontSize: "3rem" }}>
+                {/* Skeleton for the logo */}
+
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Grid>
+                <Grid item
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {/* Skeleton for the team names */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    
+                    style={{ marginTop: "8px" ,paddingRight:'10'}}
+                  />
+
+                  {/* Skeleton for the competition name */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    style={{ marginTop: "8px" }}
+                  />
+                </Grid>
+                <Grid item
+                xs={12}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                
+                }}
+                >
+                {/* Skeleton for the match time */}
+                <Skeleton
+                  variant="text"
+                  width={280}
+                  height={12}
+                  style={{ marginTop: "4px" }}
+                />
+
+                {/* Skeleton for the Predict button */}
+                <Skeleton
+                  variant="rectangular"
+                  width={280}
+                  height={36}
+                  style={{ marginTop: "8px" }}
+                />
+                </Grid>
               </CardContent>
             </StyledCard>
           </Grid>
-        ))}
-      </Grid>
-      <Typography variant="h6" align="center" gutterBottom>
-        {tomorrow}
-      </Typography>
-      <Snackbar
-        open={showPredictionToast}
-        autoHideDuration={3500}
-        onClose={handleClosePredictionToast}
-      >
-        <MuiAlert
-          severity={error ? "error" : "success"}
+        </>
+      ) : (
+        <>
+          <Grid container spacing={2} justify="center">
+            {matches.map((match) => (
+              <Grid item key={match.matchId}>
+                <StyledCard>
+                  <CardContent
+                    sx={{ display: "flex", flexDirection: "column" }}
+                  >
+                    <Grid container alignItems="center" spacing={2}>
+                      <Grid item>
+                        <LogoImage
+                          src={match.homeTeamLogo}
+                          alt={match.homeTeamName}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="h6" component="h2">
+                          {match.homeTeamName} vs {match.awayTeamName}
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <LogoImage
+                          src={match.awayTeamLogo}
+                          alt={match.awayTeamName}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Typography color="textSecondary" gutterBottom>
+                      {match.competitionName}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      {new Date(match.matchTime).toLocaleString()}
+                    </Typography>
+                    <Button
+                      sx={{ bottom: -10, width: "100%", textAlign: "center" }}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handlePrediction(match)}
+                    >
+                      Predict
+                    </Button>
+                  </CardContent>
+                </StyledCard>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+      <>
+        <Typography variant="h6" align="center" gutterBottom>
+          {tomorrow}
+        </Typography>
+        <Snackbar
+          open={showPredictionToast}
+          autoHideDuration={3500}
           onClose={handleClosePredictionToast}
-          elevation={6}
-          variant="filled"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
         >
-          {error ? error : predictionResult}
-        </MuiAlert>
-      </Snackbar>
+          <MuiAlert
+            severity={error ? "error" : "success"}
+            onClose={handleClosePredictionToast}
+            elevation={6}
+            variant="filled"
+            sx={{ fontWeigt: "600", fontFamily: "Ubuntu" }}
+          >
+            {error ? error : predictionResult}
+          </MuiAlert>
+        </Snackbar>
+      </>
     </Root>
   );
 }
