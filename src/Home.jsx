35ebaf8aeffc2a5006f26d3a2f5b17486b9a6685
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-05-17 23:34:27 +0300

    skeleteon feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -128,6 +128,9 @@ function Home() {
       {matchLoading ? (
         <>
           <Grid 
+         container
+         justify='center'
+         spacing={2}
             
           >
             <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem'}}>
@@ -413,6 +416,243 @@ function Home() {
         <Typography variant="h6" align="center" gutterBottom>
           {tomorrow}
         </Typography>
+        <>
+          <Grid 
+         container
+         justify='center'
+         spacing={2}
+            
+          >
+            <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem'}}>
+              <CardContent sx={{ fontSize: "3rem" }}>
+                {/* Skeleton for the logo */}
+
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Grid>
+                <Grid item
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {/* Skeleton for the team names */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    
+                    style={{ marginTop: "8px" ,paddingRight:'10'}}
+                  />
+
+                  {/* Skeleton for the competition name */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    style={{ marginTop: "8px" }}
+                  />
+                </Grid>
+                <Grid item
+                xs={12}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                
+                }}
+                >
+                {/* Skeleton for the match time */}
+                <Skeleton
+                  variant="text"
+                  width={280}
+                  height={12}
+                  style={{ marginTop: "4px" }}
+                />
+
+                {/* Skeleton for the Predict button */}
+                <Skeleton
+                  variant="rectangular"
+                  width={280}
+                  height={36}
+                  style={{ marginTop: "8px" }}
+                />
+                </Grid>
+              </CardContent>
+            </StyledCard>
+            <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem' }}>
+              <CardContent sx={{ fontSize: "3rem" }}>
+                {/* Skeleton for the logo */}
+
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Grid>
+                <Grid item
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {/* Skeleton for the team names */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    
+                    style={{ marginTop: "8px" ,paddingRight:'10'}}
+                  />
+
+                  {/* Skeleton for the competition name */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    style={{ marginTop: "8px" }}
+                  />
+                </Grid>
+                <Grid item
+                xs={12}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                
+                }}
+                >
+                {/* Skeleton for the match time */}
+                <Skeleton
+                  variant="text"
+                  width={280}
+                  height={12}
+                  style={{ marginTop: "4px" }}
+                />
+
+                {/* Skeleton for the Predict button */}
+                <Skeleton
+                  variant="rectangular"
+                  width={280}
+                  height={36}
+                  style={{ marginTop: "8px" }}
+                />
+                </Grid>
+              </CardContent>
+            </StyledCard>
+            <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem' }}>
+              <CardContent sx={{ fontSize: "3rem" }}>
+                {/* Skeleton for the logo */}
+
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Grid>
+                <Grid item
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {/* Skeleton for the team names */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    
+                    style={{ marginTop: "8px" ,paddingRight:'10'}}
+                  />
+
+                  {/* Skeleton for the competition name */}
+                  <Skeleton
+                    variant="text"
+                    width={260}
+                    height={24}
+                    style={{ marginTop: "8px" }}
+                  />
+                </Grid>
+                <Grid item
+                xs={12}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                
+                }}
+                >
+                {/* Skeleton for the match time */}
+                <Skeleton
+                  variant="text"
+                  width={280}
+                  height={12}
+                  style={{ marginTop: "4px" }}
+                />
+
+                {/* Skeleton for the Predict button */}
+                <Skeleton
+                  variant="rectangular"
+                  width={280}
+                  height={36}
+                  style={{ marginTop: "8px" }}
+                />
+                </Grid>
+              </CardContent>
+            </StyledCard>
+          </Grid>
+        </>
         <Snackbar
           open={showPredictionToast}
           autoHideDuration={3500}
--- src/test.js
+++ src/test.js
@@ -12,3 +12,241 @@ try{
     console.log(error)
 }
 }
+<>
+<Grid 
+sx={{display:'flex',
+flexDirection:'raw',
+raw:'wrap-wrap'
+}}
+  
+>
+  <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem'}}>
+    <CardContent sx={{ fontSize: "3rem" }}>
+      {/* Skeleton for the logo */}
+
+      <Grid
+        item
+        xs={12}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+      </Grid>
+      <Grid item
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {/* Skeleton for the team names */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          
+          style={{ marginTop: "8px" ,paddingRight:'10'}}
+        />
+
+        {/* Skeleton for the competition name */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          style={{ marginTop: "8px" }}
+        />
+      </Grid>
+      <Grid item
+      xs={12}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      
+      }}
+      >
+      {/* Skeleton for the match time */}
+      <Skeleton
+        variant="text"
+        width={280}
+        height={12}
+        style={{ marginTop: "4px" }}
+      />
+
+      {/* Skeleton for the Predict button */}
+      <Skeleton
+        variant="rectangular"
+        width={280}
+        height={36}
+        style={{ marginTop: "8px" }}
+      />
+      </Grid>
+    </CardContent>
+  </StyledCard>
+  <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem' }}>
+    <CardContent sx={{ fontSize: "3rem" }}>
+      {/* Skeleton for the logo */}
+
+      <Grid
+        item
+        xs={12}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+      </Grid>
+      <Grid item
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {/* Skeleton for the team names */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          
+          style={{ marginTop: "8px" ,paddingRight:'10'}}
+        />
+
+        {/* Skeleton for the competition name */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          style={{ marginTop: "8px" }}
+        />
+      </Grid>
+      <Grid item
+      xs={12}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      
+      }}
+      >
+      {/* Skeleton for the match time */}
+      <Skeleton
+        variant="text"
+        width={280}
+        height={12}
+        style={{ marginTop: "4px" }}
+      />
+
+      {/* Skeleton for the Predict button */}
+      <Skeleton
+        variant="rectangular"
+        width={280}
+        height={36}
+        style={{ marginTop: "8px" }}
+      />
+      </Grid>
+    </CardContent>
+  </StyledCard>
+  <StyledCard sx={{ height: 150, width: 300,marginBottom:'5rem' }}>
+    <CardContent sx={{ fontSize: "3rem" }}>
+      {/* Skeleton for the logo */}
+
+      <Grid
+        item
+        xs={12}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+        <Skeleton
+          variant="rectangular"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+      </Grid>
+      <Grid item
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {/* Skeleton for the team names */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          
+          style={{ marginTop: "8px" ,paddingRight:'10'}}
+        />
+
+        {/* Skeleton for the competition name */}
+        <Skeleton
+          variant="text"
+          width={260}
+          height={24}
+          style={{ marginTop: "8px" }}
+        />
+      </Grid>
+      <Grid item
+      xs={12}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      
+      }}
+      >
+      {/* Skeleton for the match time */}
+      <Skeleton
+        variant="text"
+        width={280}
+        height={12}
+        style={{ marginTop: "4px" }}
+      />
+
+      {/* Skeleton for the Predict button */}
+      <Skeleton
+        variant="rectangular"
+        width={280}
+        height={36}
+        style={{ marginTop: "8px" }}
+      />
+      </Grid>
+    </CardContent>
+  </StyledCard>
+</Grid>
+</>
